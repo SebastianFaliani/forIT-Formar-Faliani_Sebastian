8671a55b9579ade881158d47b59562e1d384f616
Sebastian Faliani <Sebastianfaliani@gmail.com>
2025-03-20 10:01:34 -0300

    configurando tailwind

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-orange-200">
+      <h1 className="text-4xl font-bold text-blue-600">
+        TailwindCSS está funcionando! 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
